6edd197d93772b1f0cb736b4095d01fb4824eb21
siddhant lokhande <lokhandesiddharth196@gmail.com>
2024-08-07 10:12:03 +0800

    include

---
+++ c-programming 1/blank.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+
+printf("Name: Mr.DON\ncountry:Bangladesh\n\n\ncontact: mr.don@mycompany..com");
+
+    return 0;
+}
---
+++ c-programming 1/print.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+
+    printf(" Name: Mr. X \n Univ: University of Dhaka");
+
+    return 0;
+}
