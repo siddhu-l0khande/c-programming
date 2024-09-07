df3ce0db52a1ce943424f9dea1c5a6279264e747
siddhant lokhande <lokhandesiddharth196@gmail.com>
2024-09-07 17:17:25 +0530

    include

---
+++ c_program/if.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+     int a,b;
+     printf("Enter the numbers :");
+     scanf("%d%d",&a,&b);
+
+     if (a>b)
+     {
+      printf("The number a=%d is greater ",a);
+     }
+     else{
+        printf("the number b=%d is graeter ",b);
+    
+     }
+     return 0 ;
+     
+
+}
